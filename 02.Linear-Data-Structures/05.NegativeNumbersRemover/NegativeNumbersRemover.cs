6a2e5f7bbc44724fd4a13b018f97eff35608d9cc
Stanimir <akastambata@gmail.com>
2014-08-23 16:36:50 +0300

    Adds 05.NegativeNumbersRemover.

---
+++ 02.Linear-Data-Structures/05.NegativeNumbersRemover/NegativeNumbersRemover.cs
@@ -0,0 +1,46 @@
+﻿namespace LInearDataStructures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class NegativeNumberRemover
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number or empty line for end: ");
+            string line = Console.ReadLine();
+            int number = 0;
+            List<int> numbers = new List<int>();
+
+            while (line != string.Empty)
+            {
+                try
+                {
+                    number = int.Parse(line);
+                    numbers.Add(number);
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine("Please, enter a valid string for number.");
+                }
+
+                line = Console.ReadLine();
+            }
+
+            for (int index = 0; index <= numbers.Count() - 1; index++)
+            {
+                if (numbers[index] < 0)
+                {
+                    numbers.Remove(numbers[index]);
+                    index--;
+                }
+            }
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
