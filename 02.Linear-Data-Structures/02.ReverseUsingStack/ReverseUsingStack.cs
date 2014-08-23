8d08365fe39fd89a5ec581967f71e5fe9e9dabd2
Stanimir <akastambata@gmail.com>
2014-08-23 13:27:26 +0300

    Adds 02.ReverseUsingStack.

---
+++ 02.Linear-Data-Structures/02.ReverseUsingStack/ReverseUsingStack.cs
@@ -0,0 +1,37 @@
+﻿namespace LinearDataStructures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class ReverseUsingStack
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number or empty line for end: ");
+            string line = Console.ReadLine();
+            int number = 0;
+            Stack<int> numbers = new Stack<int>();
+
+            while (line != string.Empty)
+            {
+                try
+                {
+                    number = int.Parse(line);
+                    numbers.Push(number);
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine("Please, enter a valid string for number.");
+                }
+
+                line = Console.ReadLine();
+            }
+
+            for (int index = 0; index <= numbers.Count() - 1; index++)
+            {
+                Console.WriteLine(numbers.ElementAt(index));
+            }
+        }
+    }
+}
