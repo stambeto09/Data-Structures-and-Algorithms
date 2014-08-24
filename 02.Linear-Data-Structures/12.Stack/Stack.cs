1eaaf66965d19de737a1d3a4ba4d4fb69b415c3a
Stanimir <akastambata@gmail.com>
2014-08-24 17:56:41 +0300

    Adds simulative implementation of stack.

---
+++ 02.Linear-Data-Structures/12.Stack/Stack.cs
@@ -0,0 +1,40 @@
+﻿namespace LinearDataStructures
+{
+    using System;
+
+    public class Stack<T> where T : IComparable
+    {
+        private static T[] array = new T[1];
+        private static int top;
+        private static int currentBusyIndex = -1;
+
+        public void Add(T item)
+        {
+            currentBusyIndex++;
+            top = array.Length - 1;
+            if (currentBusyIndex == top)
+            {
+                T[] newArray = new T[array.Length * 2];
+
+                for (int index = 0; index <= array.Length - 1; index++)
+                {
+                    newArray[index] = array[index];
+                }
+                
+                array = newArray;
+                Console.WriteLine("Resized");
+                Console.WriteLine("Size of the array: {0}", array.Length);
+            }
+
+            array[currentBusyIndex] = item;
+        }
+
+        public void PrintStack()
+        {
+            for (int index = top - 1; index >= 0; index--)
+            {
+                Console.WriteLine(array[index]);
+            }
+        }
+    }
+}
---
+++ 02.Linear-Data-Structures/12.Stack/StackImplementation.cs
@@ -0,0 +1,16 @@
+﻿namespace LinearDataStructures
+{
+    public class StackImplementation
+    {
+        static void Main()
+        {
+            Stack<int> myStack = new Stack<int>();
+            myStack.Add(1);
+            myStack.Add(2);
+            myStack.Add(3);
+            myStack.Add(4);
+
+            myStack.PrintStack();
+        }
+    }
+}
