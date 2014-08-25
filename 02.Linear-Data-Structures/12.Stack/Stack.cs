e0d98602a05fef712928c9f040b63c8e3ade2440
Stanimir <akastambata@gmail.com>
2014-08-25 11:46:02 +0300

    Removes currentBusyIndex.

--- 02.Linear-Data-Structures/12.Stack/Stack.cs
+++ 02.Linear-Data-Structures/12.Stack/Stack.cs
@@ -5,33 +5,28 @@
     public class Stack<T> where T : IComparable
     {
         private static T[] array = new T[1];
-        private static int top;
-        private static int currentBusyIndex = -1;
+        private static int top = -1;
 
         public void Add(T item)
         {
-            currentBusyIndex++;
-            top = array.Length - 1;
-            if (currentBusyIndex == top)
-            {
-                T[] newArray = new T[array.Length * 2];
+            top++;
+            T[] newArray = new T[array.Length * 2];
 
-                for (int index = 0; index <= array.Length - 1; index++)
-                {
-                    newArray[index] = array[index];
-                }
-                
-                array = newArray;
-                Console.WriteLine("Resized");
-                Console.WriteLine("Size of the array: {0}", array.Length);
+            for (int index = 0; index <= array.Length - 1; index++)
+            {
+                newArray[index] = array[index];
             }
 
-            array[currentBusyIndex] = item;
+            array = newArray;
+            Console.WriteLine("Resized");
+            Console.WriteLine("Size of the array: {0}", array.Length);
+
+            array[top] = item;
         }
 
         public void PrintStack()
         {
-            for (int index = top - 1; index >= 0; index--)
+            for (int index = top; index >= 0; index--)
             {
                 Console.WriteLine(array[index]);
             }
