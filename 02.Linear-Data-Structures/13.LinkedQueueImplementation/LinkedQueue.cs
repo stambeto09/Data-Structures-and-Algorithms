f8e94f3c0782d6809e6b9a48fe8e59a3f5bfb14a
Stanimir <akastambata@gmail.com>
2014-08-25 11:57:33 +0300

    Adds implementation of Linked Queue using Linked List.

---
+++ 02.Linear-Data-Structures/13.LinkedQueueImplementation/LinkedQueue.cs
@@ -0,0 +1,36 @@
+﻿namespace LinearDataStructures
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LinkedQueue<T> where T : IComparable
+    {
+        private static LinkedList<T> linkedList = new LinkedList<T>();
+        private static int top = linkedList.Count;
+
+        public void Enqueue(T item)
+        {
+            top++;
+            linkedList.AddLast(item);
+
+            Console.WriteLine("Added");
+            Console.WriteLine("Size of the list: {0}", linkedList.Count);
+        }
+
+        public void Dequeue()
+        {
+            top--;
+            linkedList.RemoveFirst();
+            Console.WriteLine("Dequeued");
+            Console.WriteLine("Size of the Linked list: {0}", linkedList.Count);
+        }
+
+        public void PrintLinkedQueue()
+        {
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 02.Linear-Data-Structures/13.LinkedQueueImplementation/LinkedQueueImplementation.cs
@@ -0,0 +1,19 @@
+﻿namespace LinearDataStructures
+{
+    class LinkedQueueImplementation
+    {
+        static void Main()
+        {
+            LinkedQueue<int> myLinkedQueue = new LinkedQueue<int>();
+            myLinkedQueue.Enqueue(1);
+            myLinkedQueue.Enqueue(2);
+            myLinkedQueue.Enqueue(3);
+            myLinkedQueue.Enqueue(4);
+            myLinkedQueue.Enqueue(5);
+            myLinkedQueue.Enqueue(6);
+
+            myLinkedQueue.Dequeue();
+            myLinkedQueue.PrintLinkedQueue();
+        }
+    }
+}
