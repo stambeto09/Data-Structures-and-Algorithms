f7067fdcc33e1f61d77193a299000fc39fcdb88f
Stanimir <akastambata@gmail.com>
2014-08-26 16:06:08 +0300

    Adds solution to 02 - Traversing of directories.

---
+++ 03.Trees-and-Traversals/TreesAndTraversals/02.TraverseDirectories/TraverseDirectory.cs
@@ -0,0 +1,45 @@
+﻿namespace TreesAndTraversals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+
+    class TraverseDirectory
+    {
+        static void Main()
+        {
+            string directoryPath = "C:\\";
+            List<string> listOfFiles = SearchDirectory(directoryPath);
+
+            foreach (var file in listOfFiles)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        private static List<string> SearchDirectory(string directoryPath)
+        {
+            List<string> listOfFiles = new List<string>();
+            try
+            {
+                foreach (string directory in Directory.GetDirectories(directoryPath))
+                {
+                    foreach (string file in Directory.GetFiles(directory, ".exe"))
+                    {
+                        listOfFiles.Add(file);
+                    }
+                    SearchDirectory(directory);
+                }
+            }
+            catch (SystemException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            return listOfFiles;
+        }
+    }
+}
