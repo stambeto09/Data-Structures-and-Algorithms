f6b543139cd2d6c8fddecbd6d3fcffa39c1ad9b2
Stanimir <akastambata@gmail.com>
2014-08-26 13:17:04 +0300

    Adds Trees and Traversal theme and solution of 01.a,b,c & d.

---
+++ 03.Trees-and-Traversals/TreesAndTraversals/01.TreeImplementation/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace Tree
+{
+    using System.Collections.Generic;
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public List<Node<T>> Children { get; set; }
+
+        public bool HasParent { get; set; }
+
+        public Node()
+        {
+            this.Children = new List<Node<T>>();
+        }
+
+        public Node(T value) : this()
+        {
+            this.Value = value;
+            this.Children = new List<Node<T>>();
+        }
+    }
+}
---
+++ 03.Trees-and-Traversals/TreesAndTraversals/01.TreeImplementation/Tree.cs
@@ -0,0 +1,124 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Tree
+    {
+        static Node<int> FindRoot(Node<int>[] nodes)
+        {
+            var hasParent = new bool[nodes.Length];
+
+            foreach (var node in nodes)
+            {
+                foreach (var child in node.Children)
+                {
+                    hasParent[child.Value] = true;
+                }
+            }
+
+            for (int index = 0; index < nodes.Length; index++)
+            {
+                if (!hasParent[index])
+                {
+                    return nodes[index];
+                }
+            }
+
+            throw new ArgumentException("nodes", "The tree does not have a root.");
+        }
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            var nodes = new Node<int>[N];
+
+            for (int index = 0; index < N; index++)
+            {
+                nodes[index] = new Node<int>(index);
+            }
+
+            for (int index = 1; index <= N - 1; index++)
+            {
+                string edgeAsString = Console.ReadLine();
+                var edgeParts = edgeAsString.Split(' ');
+
+                int parentId = int.Parse(edgeParts[0]);
+                int childId = int.Parse(edgeParts[1]);
+
+                nodes[parentId].Children.Add(nodes[childId]);
+                nodes[childId].HasParent = true;
+            }
+
+            var isChild = new bool[N];
+
+            var root = FindRoot(nodes);
+            Console.WriteLine("The root of the tree is: {0}", root.Value);
+
+            var leafs = FindAllLeafs(nodes);
+            Console.WriteLine("Leafs: ");
+            foreach (var leaf in leafs)
+            {
+                Console.Write("{0}, ", leaf.Value);
+            }
+            Console.WriteLine();
+
+            var middleNodes = FindAllMiddleNodes(nodes);
+            Console.WriteLine("Middle nodes: ");
+            foreach (var node in middleNodes)
+            {
+                Console.Write("{0}, ", node.Value);
+            }
+            Console.WriteLine(); 
+
+            // Find longest path from the root
+            var longestPath = FindLongestPath(FindRoot(nodes));
+            Console.WriteLine("Number of levels: {0}", longestPath + 1);
+        }
+
+        private static int FindLongestPath(Node<int> root)
+        {
+            if (root.Children.Count == 0)
+            {
+                return 0;
+            }
+
+            int maxPath = 0;
+            foreach (var node in root.Children)
+            {
+                maxPath = Math.Max(maxPath, FindLongestPath(node));
+            }
+
+            return maxPath + 1;
+        }
+
+        private static List<Node<int>> FindAllMiddleNodes(Node<int>[] nodes)
+        {
+            List<Node<int>> middleNodes = new List<Node<int>>();
+
+            foreach (var node in nodes)
+            {
+                if (node.HasParent && node.Children.Count > 0)
+                {
+                    middleNodes.Add(node);
+                }
+            }
+
+            return middleNodes;
+        }
+
+        private static List<Node<int>> FindAllLeafs(Node<int>[] nodes)
+        {
+            List<Node<int>> leafs = new List<Node<int>>();
+
+            foreach (var node in nodes)
+            {
+                if (node.Children.Count == 0)
+                {
+                    leafs.Add(node);
+                }
+            }
+
+            return leafs;
+        }
+    }
+}
