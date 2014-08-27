67d2489ee7a4feb3a990339af9fe75f9f7c506bd
Stanimir <akastambata@gmail.com>
2014-08-27 12:09:51 +0300

    Adds 03.FileSystem.

---
+++ 03.Trees-and-Traversals/TreesAndTraversals/03.FileSystem/File.cs
@@ -0,0 +1,14 @@
+﻿namespace FilesSystem
+{
+    class File
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+
+        public File(string name, long size)
+        {
+            this.Name = name;
+            this.Size = size;
+        }
+    }
+}
