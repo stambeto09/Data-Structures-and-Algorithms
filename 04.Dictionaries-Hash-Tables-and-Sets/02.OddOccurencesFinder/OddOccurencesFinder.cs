857529473cb1b8597b4afae7b5809a0f673912e7
Stanimir <akastambata@gmail.com>
2014-08-28 12:29:02 +0300

    Adds 02.OddOccurences in Dictionary.

---
+++ 04.Dictionaries-Hash-Tables-and-Sets/02.OddOccurencesFinder/OddOccurencesFinder.cs
@@ -0,0 +1,36 @@
+﻿namespace DictionariesHashTablesAndSets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class OddOccurencesFinder
+    {
+        static void Main()
+        {
+            IDictionary<string, int> occurences = new Dictionary<string, int>();
+            string[] words = {"C#", "SQL", "PHP", "PHP", "SQL", "SQL" };
+
+            foreach (var word in words)
+            {
+                int count = 1;
+
+                if (occurences.ContainsKey(word))
+                {
+                    count = occurences[word] + 1;
+                }
+
+                occurences[word] = count;
+            }
+
+            var oddOccurences = from element in occurences
+                                where element.Value % 2 != 0
+                                select element;
+
+            foreach (var item in oddOccurences)
+            {
+                Console.WriteLine(item.Key + " ->" + item.Value);
+            }
+        }
+    }
+}
