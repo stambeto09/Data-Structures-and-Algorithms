72c2850689af61178556f4d690c706a7ccba54d3
Stanimir <akastambata@gmail.com>
2014-08-28 13:52:10 +0300

    Adds 03.WordOccurencesInText.

---
+++ 04.Dictionaries-Hash-Tables-and-Sets/03.WordsOccurencesInText/WordsOccurencesInText.cs
@@ -0,0 +1,51 @@
+﻿namespace DictionariesHashTablesAndSets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class WordsOccurencesInText
+    {
+        static void Main()
+        {
+            StreamReader reader = new StreamReader("../../text.txt");
+            string textFromFile = reader.ReadToEnd();
+
+            string[] words = SplitWords(textFromFile);
+
+            for (int index = 0; index <= words.Length - 1; index++)
+            {
+                words[index] = words[index].ToLower();
+            }
+
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                int count = 1;
+                if (dict.ContainsKey(word))
+                {
+                    count = dict[word] + 1;
+                }
+
+                dict[word] = count;
+            }
+
+            Console.WriteLine(textFromFile);
+
+            foreach (var word in dict)
+            {
+                Console.WriteLine("{0} -> {1} times", word.Key, word.Value);
+            }
+            
+        }
+
+        private static string[] SplitWords(string textFromFile)
+        {
+            char[] separators = new char[] { '.', ',', ' ', '?', '!', ';', '-' };
+            string[] words = textFromFile.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+        }
+    }
+}
