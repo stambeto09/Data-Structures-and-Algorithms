f7161e05d1925e985824e4cb0f1b16551437c7d1
Stanimir <akastambata@gmail.com>
2014-08-29 12:02:04 +0300

    Adds Hash Table implementation.

---
+++ 04.Dictionaries-Hash-Tables-and-Sets/04.HashTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesHashTablesAndSets
+{
+    class Program
+    {
+        static void Main()
+        {
+            var table = new HashDictionary<string, int>();
+
+            table.Add("Pesho", 5);
+            table.Add("Gosho", 4);
+            table.Add("Mariika", 3);
+            table.Add("Krasimira", 2);
+            table.Add("Stamat", 1);
+
+            Console.WriteLine(table.ContainsKey("Pesho"));
+            Console.WriteLine(table.ContainsKey("Pesho2"));
+        }
+    }
+}
