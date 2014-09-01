53e5a5e5d7bb764003fd42916ffb98e113e4a5df
Stanimir <akastambata@gmail.com>
2014-09-01 13:47:34 +0300

    Adds Advanced Data structures folder & 02.ProductSearch problem related to it.

---
+++ 05.Other-Data-Structures/OtherDataStructures/02.ProductSearch/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.ProductSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Product : IComparable<Product>
+    {
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+
+
+
+        public int CompareTo(Product other)
+        {
+            return this.Value.CompareTo(other.Value);
+        }
+    }
+}
---
+++ 05.Other-Data-Structures/OtherDataStructures/02.ProductSearch/ProductSearch.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.ProductSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    class ProductSearch
+    {
+        private const int NumberOfProducts = 500000;
+        static void Main()
+        {
+            OrderedBag<Product> products = new OrderedBag<Product>();
+            Random randomNumber = new Random();
+
+            for (int i = 1; i <= NumberOfProducts; i++)
+            {
+                Product product = new Product();
+                product.Name = "Product number : " + i;
+                product.Value = randomNumber.Next(i, NumberOfProducts);
+                products.Add(product);
+            }
+
+            List<Product> productsRange = new List<Product>();
+
+            for (int index = 1; index <= 10000; index++)
+            {
+                int min = randomNumber.Next(index, 10000) / 2;
+                int max = randomNumber.Next(index, 10000) * 100;
+
+                productsRange.AddRange(products.Range(new Product(){ Value = min}, true, new Product(){Value = max}, true).Take(20));
+            }
+        }
+    }
+}
